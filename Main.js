aec05044724db65e55259b5a5057411e1461b3b7
Alejandro Nieto <alejandro.nieto@infor.com>
2018-08-31 07:54:24 -0300

    commit-1-ale

---
+++ Main.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ejemplo de JavaScript</title>
+  <meta charset="UTF-8">
+</head>
+<body>
+
+<script>
+function mostrar()
+{
+  var nom=document.getElementById('nombre').value;
+  var ed=document.getElementById('edad').value;
+  alert('Ingresó el nombre:' + nom);
+  alert('Y la edad:' + ed);
+}
+</script>
+
+<form>
+  Ingrese su nombre:
+  <input type="text" id="nombre"><br>
+  Ingrese su edad:
+  <input type="text" id="edad"><br>
+  <input type="button" value="Confirmar" onClick="mostrar()">
+</form>
+</body>
+</html>
